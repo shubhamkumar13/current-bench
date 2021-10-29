82b398fb16d7cab69fd124d65faed71e41e0f391
shubhamkumar13 <kumar.shubham1312@gmail.com>
2021-10-29 07:15:52 +0530

    Remove check_pread_log from json_util

--- pipeline/lib/json_util.ml
+++ pipeline/lib/json_util.ml
@@ -5,9 +5,6 @@ let stream_to_list stream =
 
 let parse_many string = stream_to_list (Yojson.Safe.stream_from_string string)
 
-open Current.Syntax
-module Re = Pcre
-
 let get_benchmark_name json =
   json |> Yojson.Safe.Util.(member "name") |> Yojson.Safe.Util.to_string_option
 
@@ -29,19 +26,3 @@ let validate_json json_list =
           Hashtbl.add tbl benchmark_name results)
     json_list;
   tbl
-
-let get_matches rex str =
-  Re.exec_all ~rex str
-  |> Array.map @@ Re.get_opt_substrings ~full_match:true
-
-let get_complete_match rex str =
-  let arr = get_matches rex str in
-  (* first match is the substring with absolute regexp match *)
-  match arr.(0).(0) with
-  | None -> raise @@ Failure "no complete match found"
-  | Some s -> s
-
-let check_pread_log (s : string Current.t) : string Current.t = 
-  let* s = s in
-  let r = Re.regexp {|\{(?:[^{}]|(\{(?:[^{}]|(\{(?:[^{}]|())+\}))+\}))+\}|} in
-  Current.return (get_complete_match r s)
--- pipeline/lib/json_util.mli
+++ pipeline/lib/json_util.mli
@@ -1,5 +1,3 @@
 val parse_many : string -> Yojson.Safe.t list
 
 val validate_json : Yojson.Safe.t list -> (string option, Yojson.Safe.t list) Hashtbl.t
-
-val check_pread_log : string Current.t -> string Current.t
